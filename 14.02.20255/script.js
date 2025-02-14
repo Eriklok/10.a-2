38919cf2b78fc08a8fd21f9e90971571a16f03ee
Eriklok <108319109+Eriklok@users.noreply.github.com>
2025-02-14 10:21:16 +0200

    qeqe

---
+++ 14.02.20255/script.js
@@ -0,0 +1,29 @@
+/*
+alert("ROnaldo=6 letters Goaaat=6 letters");
+
+console.log("Hello World!");
+*/
+/*
+let x = 5;
+let y = 5;
+
+if(x == y)
+{
+ console.log("x ir vienads ar y");
+}
+*/
+
+let number =prompt("enter number");
+if(number>0)
+{
+ console.log("number,ir goat")
+}
+
+if(number<0)
+{
+    console.log(number,"ir Negativs");
+}
+if (number==0)
+{
+ console.log(number,"ir 0");
+}
