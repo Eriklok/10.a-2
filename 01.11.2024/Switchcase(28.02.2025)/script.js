955a6fe8ec8a40d49b9bc6fdc7569a96bc2e5943
Eriklok <108319109+Eriklok@users.noreply.github.com>
2025-02-28 09:36:29 +0200

    nICE

---
+++ 01.11.2024/Switchcase(28.02.2025)/script.js
@@ -0,0 +1,118 @@
+/*
+let m="augusts";
+
+
+switch(m)
+{
+case "janvāris":
+    console.log("ziema");
+    break;
+
+    case "februaris":
+    console.log("ziema");
+    break;
+
+    case "marts":
+    console.log("pavasaris");
+    break;
+
+    case "aprilis":
+    console.log("pavasaris");
+    break;
+
+    case "maijs":
+    console.log("pavasaris");
+    break;
+
+    case "junijs":
+    console.log("vasara");
+    break;
+
+    case "julijs":
+    console.log("vasara");
+    break;
+
+    case "augusts":
+    console.log("vasara");
+    break;
+
+    case "septembris":
+    console.log("rūdens");
+    break;
+
+    case "oktobris":
+    console.log("rudens");
+    break;
+
+    case "novembris":
+    console.log("rudens");
+    break;
+
+    case "decembris":
+    console.log("ziema");
+    break;
+}
+    */
+
+/*
+let m="sestdiena"
+
+switch(m)
+{
+    case "pirmdiena" : 
+    case "otrdiena"  :
+    case "trešdiena"  :
+    case "ceturdiena"  :
+    case "piektdiena"  :
+    console.log("Darba diena");
+    break;
+
+    case "sestdiena":  
+    case "svetdiena"  :
+    console.log("Brīvdiena");
+    break;
+    */
+
+    /*
+let m="10"
+switch(m)
+{
+    case "1" : 
+    case "2"  :
+    case "3" :
+    case "4" :
+    console.log("slikta atzīme");
+    break;
+
+    case "5" : 
+    case "6"  :
+    case "7" :
+    console.log("vidēja atzīme");
+    break;
+
+    case "8" : 
+    case "9"  :
+    case "10" :
+    console.log("laba atzīme");
+    break;
+
+    
+}
+    */
+
+
+let m= prompt("ievadi valutu: ");
+switch(m)
+{
+    case "USD" : 
+    console.log("Amerikas dolārs");
+    break;
+
+    case "EUR" : 
+    console.log("Eiro");
+    break;
+
+    case "GBP" : 
+    console.log("Lielbritānijas mārciņa");
+    break;
+}      
