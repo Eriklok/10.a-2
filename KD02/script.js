b59c872b39cedef9e4121807b22c07c48ba7005d
Eriklok <108319109+Eriklok@users.noreply.github.com>
2025-04-25 10:11:29 +0300

    pls
    .

---
+++ KD02/script.js
@@ -0,0 +1,75 @@
+/*
+let vards = "Jānis";
+let vecums = 25;
+let irPilsonis = true;
+
+console.log("Vārds:", vards);
+console.log("Vecums:", vecums);
+console.log("Ir pilsonis:", irPilsonis);
+
+
+if (vecums >= 18 && irPilsonis) {
+    console.log("Drīkst balsot.");
+} else if (vecums >= 18 && !irPilsonis) {
+    console.log(`Personai ir ${vecums} gadi, bet viņa nav pilsonis.`);
+} else if (vecums < 18 && irPilsonis) {
+    console.log(`Personai ir ${vecums} gadi, bet viņa ir pilsonis.`);
+} else {
+    console.log(`Personai ir ${vecums} gadi, un viņa nav pilsonis.`);
+}
+*/
+
+ /*
+let atzime = prompt("Ievadiet atzīmi (0-100):");
+
+switch (true) {
+    case atzime >= 90 && atzime <= 100:
+        console.log("Izcili");
+        break;
+    case atzime >= 70 && atzime <= 89:
+        console.log("Labi");
+        break;
+    case atzime >= 50 && atzime <= 69:
+        console.log("Vidēji");
+        break;
+    case atzime >= 0 && atzime <= 49:
+        console.log("Slikti");
+        break;
+}
+*/
+/*
+let saraksts = [];
+
+saraksts.push("Ābols");
+saraksts.push("Banāns");
+saraksts.push("Apelsīns");
+saraksts.push("Vīnogas");
+saraksts.push("Bumbieris");
+
+for (let i = 0; i < saraksts.length; i++) {
+    console.log("Masīva elements:", saraksts[i]);
+}
+
+*/
+
+/*
+let saraksts = [];
+
+saraksts.push("Ābols");
+saraksts.push("Banāns");
+saraksts.push("Apelsīns");
+saraksts.push("Vīnogas");
+saraksts.push("Bumbieris");
+
+for (let i = saraksts.length - 1; i >= 0; i--) {
+    console.log("Masīva elements apgrieztā secībā:", saraksts[i]);
+}
+
+*/
+
+
+
+
+
+
+
